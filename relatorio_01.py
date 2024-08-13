bf524ed66be4f8ccf2565a420de1c248cdee621c
zecarn <84153579+zecarn@users.noreply.github.com>
2024-08-12 23:13:04 -0300

    Add files via upload

---
+++ relatorio_01.py
@@ -0,0 +1,37 @@
+class Professor:
+    def __init__(self, nome):
+        self.nome = nome
+    
+    def ministrar_aula(self, assunto):
+        print(f"O professor {self.nome} está ministrando uma aula sobre {assunto}")
+
+class Aluno:
+    def __init__(self, nome):
+        self.nome = nome
+    
+    def presenca(self):
+        return f"O aluno {self.nome} esta presente."
+
+class Aula:
+    def __init__(self, professor, assunto):
+        self.professor = professor
+        self.assunto = assunto
+        self.alunos = []
+    
+    def adicionar_aluno(self, aluno):
+        self.alunos.append(aluno)
+
+    def listar_presenca(self):
+        presenca = f"Presenca na aula sobre {self.assunto}, ministrada pelo professor {self.professor.nome}: \n"
+        for aluno in self.alunos:
+            presenca += aluno.presenca() + "\n"
+
+        return presenca
+
+professor = Professor("Lucas")
+aluno1 = Aluno("Maria")
+aluno2 = Aluno("Pedro")
+aula = Aula(professor, "Programacao Orientada a Objetos")
+aula.adicionar_aluno(aluno1)
+aula.adicionar_aluno(aluno2)
+print(aula.listar_presenca())
